52170d7ba1b1e132043500f62c5a77c7d7c3417f
Eugene van Aubel <35023@ma-web.nl>
2022-12-15 11:41:44 +0100

    Added some stuff on my folders

--- Eugene's folder/js/script.js
+++ Eugene's folder/js/script.js
@@ -1,4 +1,3 @@
-console.log("Logged")
 function facebook() {
     window.open("http://www.facebook.com/nationaalgamemuseum");
   }
@@ -28,41 +27,19 @@ function facebook() {
   let imageContainer = document.getElementsByClassName("slideshow-container")[0];
   let images = imageContainer.querySelectorAll('img');
   let current = 0;
-console.log(images);
-  // Set first image to visible
   images[current].classList.remove('hidden');
-  console.log(current, images[current].classList)
-
-  // Add event listener for next button
   document.getElementById('next').addEventListener('click', 
   function() {
-    // Hide current image
-  console.log(current, images[current].classList)
     images[current].classList.add('hidden');
-  console.log(current, images[current].classList)
-
-    // Increment current image
-   // current = (current + 1) % images.length;
     current++;
     if(current >= images.length)
     {
       current=0;
     }
-
-    // Show new image
-  console.log(current, images[current].classList)
     images[current].classList.remove('hidden');
-  console.log(current, images[current].classList)
   });
-
-  // Add event listener for previous button
   document.getElementById('prev').addEventListener('click', function() {
-    // Hide current image
     images[current].classList.add('hidden');
-
-    // Decrement current image
     current = (current + images.length - 1) % images.length;
-
-    // Show new image
     images[current].classList.remove('hidden');
   });
